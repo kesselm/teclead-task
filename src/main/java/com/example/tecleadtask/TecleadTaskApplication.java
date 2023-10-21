d0044d4c16cc613a506f8f95eb102a8c62ee61b8
sifu <info@kessel.site>
2023-10-22 00:22:42 +0200

    Adding title to swagger documentation.

--- src/main/java/com/example/tecleadtask/TecleadTaskApplication.java
+++ src/main/java/com/example/tecleadtask/TecleadTaskApplication.java
@@ -1,9 +1,12 @@
 package com.example.tecleadtask;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Teclead Task Api", version = "1.0", description = "A simple CRUD application with REST."))
 public class TecleadTaskApplication {
 
     public static void main(String[] args) {
