eb4adbff86be206041e37db7cd0bf7e08b244b0d
sifu <info@kessel.site>
2023-11-18 14:15:23 +0100

    Adding actuator dependency and improving swagger ui.

--- src/main/java/com/example/tecleadtask/config/ApplicationConfig.java
+++ src/main/java/com/example/tecleadtask/config/ApplicationConfig.java
@@ -38,36 +38,6 @@ public class ApplicationConfig {
     @Bean
     public OpenAPI openAPI() throws JSONException {
 
-        JSONObject findUser = new JSONObject();
-        findUser.put("href", "http://localhost:8080/api/v1/users/827");
-
-        JSONObject user = new JSONObject();
-        user.put("id", 827);
-        user.put("name", "Keßel");
-        user.put("vorname", "Martin");
-        user.put("email", "martin@kessel.de");
-        user.put("_links", findUser);
-
-        JSONArray users = new JSONArray();
-        users.put(user);
-
-        JSONObject embedded = new JSONObject();
-        embedded.put("Users", users);
-
-        JSONObject before = new JSONObject();
-        before.put("href", "http://localhost:8080/api/v1/users?page149&size=1&field=vorname&sortAlg=ASC");
-
-        JSONObject next = new JSONObject();
-        next.put("href", "http://localhost:8080/api/v1/users?page151&size=1&field=vorname&sortAlg=ASC");
-
-        JSONObject links = new JSONObject();
-        links.put("before", before);
-        links.put("next", next);
-
-        var response = new Schema<Map<String, Object>>()
-                .addProperty("_embedded", new JsonSchema().example(embedded.toString()))
-                .addProperty("_links", new JsonSchema().example(links.toString()));
-
         var devServer = new Server();
         devServer.setUrl("http://localhost:8080");
         devServer.setDescription("Server URL in Development environment");
@@ -87,8 +57,101 @@ public class ApplicationConfig {
                 .contact(contact)
                 .description("This API exposes endpoints to teclead task app.");
 
-        return new OpenAPI().info(info).servers(List.of(devServer, prodServer)).components(new Components()
-                .addSchemas("Collection", response)
+        return new OpenAPI().info(info).servers(List.of(devServer, prodServer))
+                .components(new Components()
+                .addSchemas("Users", createSchemaForUsers())
+                .addSchemas("UserResponse", new JsonSchema().example(createUser().toString()))
+                .addSchemas("UserRequest", new JsonSchema().example(createUpdateUserRequestBody().toString()))
         );
+    }
+
+    private JSONArray createUsers() {
+        JSONArray users = new JSONArray();
+        users.put(createUser());
+        return users;
+    }
+
+    private JSONObject createUser() {
+        JSONObject findUser = new JSONObject();
+        findUser.put("href", "http://localhost:8080/api/v1/users/827");
+        findUser.put("type", "GET");
+
+        JSONObject deleteUser = new JSONObject();
+        deleteUser.put("href","http://localhost:8080/api/v1/user/713");
+        deleteUser.put("type", "DELETE");
+
+        JSONObject updateUser = new JSONObject();
+        updateUser.put("href","http://localhost:8080/api/v1/user");
+        updateUser.put("type", "PUT");
+
+        JSONObject findUserByVorname = new JSONObject();
+        findUserByVorname.put("href", "http://localhost:8080/api/v1/user?vorname=Martin");
+        findUserByVorname.put("type", "GET");
+
+        JSONObject userLinks = new JSONObject();
+        userLinks.put("findUser", findUser);
+        userLinks.put("deleteUser", deleteUser);
+        userLinks.put("updateUser", updateUser);
+        userLinks.put("findUserByVorname", findUserByVorname);
+
+        JSONObject user = new JSONObject();
+        user.put("id", 827);
+        user.put("name", "Keßel");
+        user.put("vorname", "Martin");
+        user.put("email", "martin@kessel.de");
+        user.put("_links", userLinks);
+        return user;
+    }
+
+    private Schema createSchemaForUsers() {
+        JSONObject embedded = new JSONObject();
+        embedded.put("Users", createUsers());
+
+        JSONObject first = new JSONObject();
+        first.put("href", "http://localhost:8080/api/v1/users?page149&size=1&field=vorname&sortAlg=ASC");
+
+        JSONObject prev = new JSONObject();
+        prev.put("href", "http://localhost:8080/api/v1/users?page=149&size=5&sort=vorname,asc");
+
+        JSONObject self = new JSONObject();
+        self.put("href", "http://localhost:8080/api/v1/users?page=150&size=5&sort=vorname,asc");
+
+        JSONObject next = new JSONObject();
+        next.put("href", "http://localhost:8080/api/v1/users?page=151&size=5&sort=vorname,asc");
+
+        JSONObject last = new JSONObject();
+        last.put("href", "http://localhost:8080/api/v1/users?page=199&size=5&sort=vorname,asc");
+
+        JSONObject saveUser = new JSONObject();
+        saveUser.put("href", "http://localhost:8080/api/v1/user");
+        saveUser.put("type", "POST");
+
+        JSONObject page = new JSONObject();;
+        page.put("size", 5);
+        page.put("totalElement", 1000);
+        page.put("totalPage", 200);
+        page.put("number", 150);
+
+        JSONObject links = new JSONObject();
+        links.put("first", first);
+        links.put("prev", prev);
+        links.put("self", self);
+        links.put("next", next);
+        links.put("last", last);
+        links.put("saveUser", saveUser);
+
+        return new Schema<Map<String, Object>>()
+                .addProperty("_embedded", new JsonSchema().example(embedded.toString()))
+                .addProperty("_links", new JsonSchema().example(links.toString()))
+                .addProperty("page", new JsonSchema().example(page.toString()));
+    }
+
+    private JSONObject createUpdateUserRequestBody() {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("id",1);
+        requestBody.put("name","Keßel");
+        requestBody.put("vorname","Martin");
+        requestBody.put("email", "martinkessel@web.de");
+        return requestBody;
     }
 }
--- src/main/java/com/example/tecleadtask/controllers/UserController.java
+++ src/main/java/com/example/tecleadtask/controllers/UserController.java
@@ -9,7 +9,6 @@ import com.example.tecleadtask.util.EntityConverter;
 import com.example.tecleadtask.util.SortAlgorithm;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -29,8 +28,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.example.tecleadtask.util.ApiConstants.*;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Tag(name = "Teclead Task Application", description = "An Application to find users.")
 @RestController
@@ -41,24 +38,27 @@ public class UserController {
     private final UserService userService;
     private final UserModelAssembler userModelAssembler;
 
-    private final String host = "http://localhost:8080/api/v1/";
-
     @Operation(
             summary = "Create a new user entity.",
             description = "Create a new user entity.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "User to update.",
+                    content = @Content(
+                            schema = @Schema(ref = "#/components/schemas/UserRequest"))),
             responses = {
-                    @ApiResponse(responseCode = "201", description = "Created",
-                            content = {@Content(schema = @Schema(implementation = UserDTO.class),
-                                    mediaType = "application/json")}),
+                    @ApiResponse(responseCode = "201", description = "Created", content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(ref = "#/components/schemas/UserResponse")
+                            )
+                    }),
                     @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
                     @ApiResponse(responseCode = "500", description = "Server Error", content = @Content),
             })
     @PostMapping(SAVE_USER)
     public HttpEntity<UserDTO> saveUser(@Valid @RequestBody UserDTO userDTO) {
         UserEntity userEntity = userService.saveUser(EntityConverter.convertFromUserDTO(userDTO));
-        UserDTO userDto = EntityConverter.convertFromUserEntity(userEntity);
-        userDto.add(linkTo(methodOn(UserController.class).saveUser(userDTO)).withSelfRel());
-        return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+        return new ResponseEntity<>(userModelAssembler.toModel(userEntity), HttpStatus.CREATED);
     }
 
     @Operation(
@@ -68,16 +68,16 @@ public class UserController {
                     @ApiResponse(responseCode = "200", content = {
                             @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(ref = "#/components/schemas/Collection")
+                                    schema = @Schema(ref = "#/components/schemas/Users")
                             )
                     }),
                     @ApiResponse(responseCode = "204", description = "No Content", content = @Content)
             })
     @GetMapping(FIND_USERS)
     public ResponseEntity<PagedModel<UserDTO>> findPaginatedUsers(@Parameter(description = "Number of the result set page.") @RequestParam(value = "page", defaultValue = "0") int page,
-                                                                       @Parameter(description = "Number of results on the page.") @RequestParam(value = "size", defaultValue = "10") int size,
-                                                                       @Parameter(description = "Field sort criteria.") @RequestParam(value = "field", defaultValue = "vorname") String field,
-                                                                       @Parameter(description = "Sort algorithm.") @RequestParam(value = "sortAlg", defaultValue = "ASC") SortAlgorithm sortAlg) {
+                                                                  @Parameter(description = "Number of results on the page.") @RequestParam(value = "size", defaultValue = "10") int size,
+                                                                  @Parameter(description = "Field sort criteria.") @RequestParam(value = "field", defaultValue = "vorname") String field,
+                                                                  @Parameter(description = "Sort algorithm.") @RequestParam(value = "sortAlg", defaultValue = "ASC") SortAlgorithm sortAlg) {
 
         Sort sort = Sort.by(field);
         if (sortAlg.equals(SortAlgorithm.ASC)) {
@@ -96,35 +96,41 @@ public class UserController {
         }
     }
 
-    @GetMapping("/get")
-    public List<UserEntity> getAll() {
-        return userService.findAllUsers();
-    }
-
     @Operation(
             summary = "Get a user account by identifier.",
             description = "Get a user account by identifier.",
             responses = {
-                    @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = UserDTO.class), mediaType = "application/json")}),
-                    @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+                    @ApiResponse(responseCode = "200", content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(ref = "#/components/schemas/UserResponse")
+                            )
+                    }),
+                    @ApiResponse(responseCode = "204", description = "No Content", content = @Content(
+                            schema = @Schema()
+                    )),
                     @ApiResponse(responseCode = "500", description = "Server Error", content = @Content),
             })
     @GetMapping(FIND_USER_BY_ID)
     public ResponseEntity<UserDTO> findUserById(@Parameter(description = "Id of user to be searched.") @PathVariable("id") Long id) {
         Optional<UserEntity> user = userService.findUserById(id);
 
-        return user.map(value -> new ResponseEntity<>(EntityConverter.convertFromUserEntity(value), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+        return user.map(value -> new ResponseEntity<>(userModelAssembler.toModel(value), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
     }
 
     @Operation(
             summary = "Delete an user.",
             description = "Delete a user.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "User to delete.",
+                    content = @Content(
+                            schema = @Schema(ref = "#/components/schemas/UserRequest"))),
             responses = {
-                    @ApiResponse(responseCode = "204", description = "No Content", content = {@Content(schema = @Schema(implementation = UserDTO.class), mediaType = "application/json")}),
-                    @ApiResponse(responseCode = "500", description = "Server Error", content = @Content)
+                    @ApiResponse(responseCode = "204", description = "No Content", content = {@Content(schema = @Schema())}),
+                    @ApiResponse(responseCode = "500", description = "Server Error", content = {@Content(schema = @Schema())})
             })
     @DeleteMapping(ApiConstants.DELETE_USER)
-    public ResponseEntity deleteUser(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<Void> deleteUser(@RequestBody UserDTO userDTO) {
         userService.deleteUser(EntityConverter.convertFromUserDTO(userDTO));
         return ResponseEntity.noContent().build();
     }
@@ -133,11 +139,11 @@ public class UserController {
             summary = "Delete a user by identifier.",
             description = "Delete a user by identifier.",
             responses = {
-                    @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = UserDTO.class), mediaType = "application/json")}),
-                    @ApiResponse(responseCode = "500", description = "Server Error", content = @Content),
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(responseCode = "500", description = "Server Error"),
             })
     @DeleteMapping(ApiConstants.DELETE_USER_BY_ID)
-    public ResponseEntity deleteUserById(@Parameter(description = "Id of user to be searched.") @PathVariable Long id) {
+    public ResponseEntity<Void> deleteUserById(@Parameter(description = "Id of user to be searched.") @PathVariable Long id) {
         userService.deleteUserById(id);
         return ResponseEntity.noContent().build();
     }
@@ -145,25 +151,31 @@ public class UserController {
     @Operation(
             summary = "Update an user.",
             description = "Update an user.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "User to update.",
+                    content = @Content(
+                            schema = @Schema(ref = "#/components/schemas/UserRequest"))),
             responses = {
-                    @ApiResponse(responseCode = "204", description = "No Content", content = {@Content(schema =
-                    @Schema(implementation = UserDTO.class), mediaType = "application/json")}),
-                    @ApiResponse(responseCode = "500", description = "Server Error", content = @Content),
+                    @ApiResponse(responseCode = "200", description = "ok", content = {@Content(schema = @Schema())}),
+                    @ApiResponse(responseCode = "500", description = "Server Error", content = {@Content(schema = @Schema())}),
             })
-    @ResponseStatus(HttpStatus.OK)
     @PutMapping(ApiConstants.UPDATE_USER)
-    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO) {
         UserEntity userEntity = EntityConverter.convertFromUserDTO(userDTO);
         UserEntity updatedUserEntity = userService.updateUser(userEntity);
-        return EntityConverter.convertFromUserEntity(updatedUserEntity);
+        return ResponseEntity.ok(userModelAssembler.toModel(updatedUserEntity));
     }
 
     @Operation(
-            summary = "Get user entities related to the vorName.",
-            description = "Get user entities related to the vorName.",
+            summary = "Get user entities related to the Vorname.",
+            description = "Get user entities related to the Vorname.",
             responses = {
-                    @ApiResponse(responseCode = "200", content = {@Content(array = @ArraySchema(
-                            schema = @Schema(implementation = UserDTO.class)), mediaType = "application/json")}),
+                    @ApiResponse(responseCode = "200", content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(ref = "#/components/schemas/Users")
+                            )
+                    }),
                     @ApiResponse(responseCode = "204", content = {@Content(schema = @Schema())})
             })
     @GetMapping(ApiConstants.FIND_USER_BY_VORNAME)
